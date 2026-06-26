97eb1ce4cd6cc5f74740e1da6edd56845b11a16e
Adityasinh Raulji <127715890+Ad1ty451nh@users.noreply.github.com>
2026-06-26 20:49:39 +0530

    Update FizzBuzz.swift

--- FizzBuzz.swift
+++ FizzBuzz.swift
@@ -1,19 +1,22 @@
-func FizzBuzz(_ n: Int){
-    var result: [String] = []
+Problem Link: https://leetcode.com/problems/fizz-buzz/
 
-    for i in 1...n {
 
-        if i % 3 == 0 && i % 5 == 0 {
-            print("FizzBuzz")
-        } else if i % 3 == 0{
-            print("Fizz")
-        } else if i % 5 == 0 {
-            print("Buzz")
-        }else {
-            print(i)
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var ans: [String] = []
+
+        for i in 1...n {
+            if i % 3 == 0 && i % 5 == 0 {
+                ans.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                ans.append("Fizz")
+            } else if i % 5 == 0 {
+                ans.append("Buzz")
+            } else {
+                ans.append(String(i))
+            }
         }
+
+        return ans
     }
 }
-
-var n = 30
-FizzBuzz(n)
