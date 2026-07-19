e39390fca46d40a67bdff4d2173109bf873b2859
Adityasinh Raulji <127715890+Ad1ty451nh@users.noreply.github.com>
2026-07-19 22:50:46 +0530

    Add targetIndices method in Solution class
    Implement method to find target indices after sorting.

---
+++ 2089.java
@@ -0,0 +1,25 @@
+// problem link: https://leetcode.com/problems/find-target-indices-after-sorting-array
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        int num = 0;
+        int tcount = 0;
+
+        for(int ele : nums){
+            if (ele == target){
+                tcount++;
+            } else if(ele < target){
+                num++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<> ();
+
+        while(tcount > 0){
+            ans.add(num);
+            num++;
+            tcount--;
+        }
+        return ans;
+    }
+}
