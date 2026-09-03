1e32a27522f6e8662028be7ef1b14d6e565b183f
Adityasinh Raulji <127715890+Ad1ty451nh@users.noreply.github.com>
2026-09-03 22:14:49 +0530

    Create 1334.java

---
+++ 1334.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+
+        int[][] dist = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+
+            dist[u][v] = weight;
+            dist[v][u] = weight;
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+
+                    if (dist[i][k] != Integer.MAX_VALUE &&
+                        dist[k][j] != Integer.MAX_VALUE) {
+
+                        dist[i][j] = Math.min(
+                            dist[i][j],
+                            dist[i][k] + dist[k][j]
+                        );
+                    }
+                }
+            }
+        }
+
+        int answer = -1;
+        int minCount = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && dist[i][j] <= distanceThreshold) {
+                    count++;
+                }
+            }
+
+            if (count <= minCount) {
+                minCount = count;
+                answer = i;
+            }
+        }
+
+        return answer;
+    }
+}
